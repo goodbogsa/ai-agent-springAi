94916697f6cffab99bf9f8bad83333fb0a498fb5
lunzhentao <2232885917@qq.com>
2026-04-16 11:29:03 +0800

    11

---
+++ src/main/java/org/example/aiagentspringbootai/AiAgentSpringbootaiApplication.java
@@ -0,0 +1,13 @@
+package org.example.aiagentspringbootai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAgentSpringbootaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAgentSpringbootaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aiagentspringbootai/AiAgentSpringbootaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aiagentspringbootai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentSpringbootaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
